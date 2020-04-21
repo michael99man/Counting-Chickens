737a6e9204fa82b54339a4ef3419730c3cf29a87
Michael Man <17michaelm@gmail.com>
2020-04-21 02:00:35 -0400

    init

---
+++ src/Chicken.java
@@ -0,0 +1,53 @@
+import java.util.Random;
+
+public class Chicken {
+
+    int level;
+    int cost;
+    boolean alive = true;
+    Random r;
+
+    // starting from nothing, generate a chicken of a given level and compute the total cost (min bet size of 100)
+    public Chicken(int targetLevel) {
+        r = new Random();
+        int cost = 100;
+        int level = 50;
+
+        assert(level >= 50 && level <= 70);
+
+        while(true){
+
+            if(level == targetLevel) break;
+            int roll = 1 + r.nextInt(100);
+
+            // we win the fight
+            if(roll <= level){
+                level++;
+                // we win 100 bucks
+                cost -= 100;
+            } else {
+                // lose the fight and buy a new level 50 chicken
+                cost += 200;
+                level = 50;
+            }
+        }
+
+        this.cost = cost;
+        this.level = level;
+    }
+
+    public int fight(int bet){
+        assert(alive);
+        assert(false);
+
+        int roll = 1 + r.nextInt(100);
+
+        if(roll <= level){
+            level++;
+            return bet;
+        } else {
+            alive = false;
+            return -1*bet;
+        }
+    }
+}
---
+++ src/Fighter.java
@@ -0,0 +1,59 @@
+public class Fighter {
+
+    public static final int WEALTH_CEILING = 2000000;
+
+    public static void main(String[] args) {
+        testBasicStrategy(10000);
+    }
+
+    public static void testBasicStrategy(int nTrials){
+        int initialWealth = 200000;
+
+        // test bet sizes in 1k increments up to 10k
+        for(int betSize = 1000; betSize<= 10000; betSize+=1000) {
+            int hitMin = 0;
+
+            long totalWealth = 0;
+            for (int i = 0; i < nTrials; i++) {
+                int diff = runBasicStrategy(initialWealth, betSize);
+                totalWealth += diff;
+
+                if (diff == 0) hitMin++;
+            }
+
+            System.out.printf("EV of basic strategy with bet size %d: %f\n", betSize, totalWealth / 1.0 / nTrials);
+            System.out.printf("Busts: %f percent of the time\n", hitMin * 100.0 / nTrials);
+        }
+    }
+
+
+    // continuously generate level 51 chickens and bet 10,000 on them (or their higher values)
+    public static int runBasicStrategy(int wealth, int defaultBetSize){
+        Chicken cock = new Chicken(51);
+        wealth -= cock.cost;
+
+
+        while (wealth >= 0 && wealth <= WEALTH_CEILING){
+            int bet = (int) Math.min(defaultBetSize * (1.00 + 0.3*(cock.level - 51)), wealth);
+            wealth += cock.fight(bet);
+
+            // busted
+            if(wealth == 0){
+                break;
+            }
+
+            if(!cock.alive){
+                cock = new Chicken(51);
+                if(cock.cost > wealth) {
+                    // couldn't afford new chicken
+                    wealth = 0;
+                } else {
+                    wealth -= cock.cost;
+                }
+            }
+        }
+
+        //System.out.println("Ended with wealth: " + wealth);
+        return wealth;
+    }
+}
